c244d9b4bc720c49eae7d91caf3d878c93646a00
Ketil Malde <ketil@malde.org>
2013-04-30 15:39:58 +0200

    update indexing functionality
    Ignore-this: e542441a9c07c9951663196f574d3a18
    
    darcs-hash:48424eca2766ee4d6b1a66c6d088c18cb86b5397

--- META/scriptindex.sh
+++ META/scriptindex.sh
@@ -3,12 +3,15 @@
 DIR=/data/genomdata
 
 for a in `find $DIR -name meta.xml`; do 
-  echo url=`dirname $a`
+  name=`dirname $a`
+  echo url="http://nmdbio"$name
+  echo name=`basename $name`
   xmlstarlet sel -t -m "//species" -o "species= " -v "@sciname" -o "     " -v "@tsn" -o "        " -v "." -n $a | grep -v "^$"
   xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
-  echo "sample= "`cat $a`
+  echo "sample= "`xmlstarlet sel -t -m "//description" -v "." $a`
   echo
 done | scriptindex -v --overwrite $DIR/META/metadb $DIR/META/index.def
 
 rm -rf /var/lib/xapian-omega/data/default                     
 cp -a $DIR/META/metadb /var/lib/xapian-omega/data/default       
+# sshfs -o allow_other zeus:/data/genomdata /data/genomdata
