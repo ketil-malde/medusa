ea2f4f9f265bec2fcfedf6b00371e23ef180924a
Ketil Malde <ketil@malde.org>
2013-05-10 16:16:52 +0200

    use local omega database
    Ignore-this: 85ebdd87bf5f8ddd606d1522d9a33c63
    
    darcs-hash:b67762fd861e2f510e4ba7a6d39001cfddf9d5f2

--- META/scriptindex.sh
+++ META/scriptindex.sh
@@ -10,8 +10,10 @@ for a in `find $DIR -name meta.xml`; do
   xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
   echo "sample= "`xmlstarlet sel -t -m "//description" -v "." $a`
   echo
-done | scriptindex -v --overwrite $DIR/META/metadb $DIR/META/index.def
+done | scriptindex -v --overwrite $DIR/META/metadb/default $DIR/META/index.def
 
-rm -rf /var/lib/xapian-omega/data/default                     
-cp -a $DIR/META/metadb /var/lib/xapian-omega/data/default       
+# rm -rf /var/lib/xapian-omega/data/default                     
+# cp -a $DIR/META/metadb /var/lib/xapian-omega/data/default       
+
+# When using sshfs, the allow_other option must be set, like so:
 # sshfs -o allow_other zeus:/data/genomdata /data/genomdata
