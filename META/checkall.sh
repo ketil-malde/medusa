9f28a1829a78fc7309d2b046ea7ab76f326af195
Ketil Malde <ketil.malde@imr.no>
2013-07-22 14:04:51 +0200

    Output all successes and failures in red and green, resp.
    Ignore-this: c367f855b81edf30e0c69e87bc7abe02
    
    darcs-hash:69d0b9f29b57876c1754df608df633a04b340bd9

--- META/checkall.sh
+++ META/checkall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+R="$(tput setaf 1)"
+G="$(tput setaf 2)"
+
 LOG=/tmp/xmlcheck.log
 date > $LOG
 
 for a in *; do
   if echo $a | egrep -q -v _darcs\|lost\\+found\|META; then
-     META/xmlcheck.sh $a || echo "FAILED:	$a" >> $LOG
+     META/xmlcheck.sh $a && echo -n "$G$a " >> $LOG || echo -n "$R$a " >> $LOG
      echo '--------'
   fi
 done
