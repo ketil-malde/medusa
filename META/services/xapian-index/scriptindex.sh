77a441dd797d6992fb7b2d7de4ff2882f93af7be
Ketil Malde <ketil.malde@imr.no>
2013-10-01 14:13:52 +0200

    Add URLPREFIX config to scriptindex
    Ignore-this: 931e7710b01a36b2a29c2b7851cc889c
    
    darcs-hash:62d2e05d5eff2465f2ce61fd5a4f19f37a99f358

--- META/services/xapian-index/scriptindex.sh
+++ META/services/xapian-index/scriptindex.sh
@@ -2,11 +2,13 @@
 
 DIR=/data/genomdata
 TARGET_DIR=/var/lib/omega/data/default
+URLPREFIX=
 
-for a in `find $DIR -name meta.xml`; do 
-  name=`dirname $a`
-  echo url="http://nmdbio"$name
-  echo name=`basename $name`
+for a in $DIR/*/meta.xml; do 
+  path=`dirname $a`
+  name=`basename $path`
+  echo url="$URLPREFIX"/$name
+  echo name=$name
   xmlstarlet sel -t -m "//species" -o "species= " -v "@sciname" -o "     " -v "@tsn" -o "        " -v "." -n $a | grep -v "^$"
   xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
   echo "sample= "`xmlstarlet sel -t -m "//description" -v "." $a`
