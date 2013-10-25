d3d365652c5db5b2e3812c0ea65198360df54ee1
Ketil Malde <ketil@malde.org>
2013-10-25 10:27:10 +0200

    Safety issues, do not copy irrelevant files (but still copy directories, sigh)
    Ignore-this: e58d98ad8c65801496e2f1a170353b2b
    
    darcs-hash:a24fd487478ead931ae35563ab75cfbe320fab04

--- put.sh
+++ put.sh
@@ -19,17 +19,37 @@ set -euf -o pipefail
 
 error(){
     echo "Error: $*"
+    set +u
+    [ -d "$STAGE" ] && rm -rf "$STAGE"
     exit -1
+}
+
+# extract list of files from meta.xml
+getfiles(){
+    xmlstarlet sel -t -m "//file" -v "@path" -n "$1"
 }
 
 case "$MDZ_REPO_METHOD" in
     scp)
 	RPATH=$(echo $MDZ_REPOSITORY | cut -d: -f2)
 	RHOST=$(echo $MDZ_REPOSITORY | cut -d: -f1)
-	E=$(ssh $RHOST "ls -d $RPATH/$1 2> /dev/null; echo -n") || error "$0: Failed to connect to host $RHOST"
-        # BUG: Will also copy irrelevant files and incorrect (e.g. empty) datasets!
+	NAME=`basename "$1"`
+	E=$(ssh $RHOST "ls -d \"$RPATH/$NAME\" 2> /dev/null; echo -n") || error "$0: Failed to connect to host $RHOST."
+        # BUG: Will also copy irrelevant directories, don't cleanup on remote side.
 	[ -z "$E" ] || error "$1 already exists in $MDZ_REPOSITORY"
-	scp -rp "$SOURCE" "$MDZ_REPOSITORY/" || error "Failed to transfer $1 to $MDZ_REPOSITORY"
+	STAGE="/tmp/$NAME"
+	mkdir "$STAGE"
+	cp -a "$SOURCE/meta.xml" "$STAGE" || error "Couldn't stage in /tmp, is this a real dataset?"
+	cd $SOURCE
+        find . -type d | while read dir; do
+	    mkdir -p "$STAGE/$dir"
+	done
+	cd -
+	scp -rp "$STAGE" "$MDZ_REPOSITORY/" || error "Failed to transfer $1 to $MDZ_REPOSITORY"
+	for path in `getfiles $SOURCE/meta.xml`; do
+	    scp "$SOURCE/$path" "$MDZ_REPOSITORY/$NAME/$path" 
+        done
+	rm -rf "$STAGE"
     ;;
 
     *)
