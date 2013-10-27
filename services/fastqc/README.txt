f6a1a580257af0bfa52ec1fbc71d52f9df1e0785
Ketil Malde <ketil@malde.org>
2013-10-27 19:33:35 +0100

    add initial fastq
    Ignore-this: 1a596d730c494b0c01628e332c2b261a
    
    darcs-hash:e2204336cf319f0164a3c98b7c7aed4cfa41db5f

---
+++ services/fastqc/README.txt
@@ -0,0 +1,6 @@
+fastqc.sh <dataset> <output>
+
+Run the fastqc program on any fastq files in dataset,
+store the result as a new dataset in <output>.
+
+
---
+++ services/fastqc/fastqc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+error(){
+    echo "Error: $*"
+    exit -1
+}
+
+[ "$#" = "2" ] || error "Usage: fastqc <dataset> <output>"
+
+INPUT=$1
+mkdir "$2" || error "Failed to make ouptut directory \"$2\""
+cd "$2"
+TARGET=$(pwd -P)
+THREADS=`nproc`
+cd -
+
+# extract all fastq-files from input
+cd "$INPUT" || error "Couldn't navigate to input dataset \"$INPUT\""
+FILES=$(xmlstarlet sel -t -m "//file[@mimetype=\"text/x-fastq\"]" -v "@path" -n meta.xml)
+# run fastq
+fastqc -t "$THREADS" -o "$TARGET" "$FILES"
+# generate metadata
+
+# link to dataset-id
+# link to file names
+# build summary page?
+
+cd -
+
