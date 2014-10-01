aa45955692cd11c27673840c0b54880f0b09fa76
Ketil Malde <ketil@malde.org>
2014-10-01 10:09:38 +0200

    Reworked variables, set appropriate defaults for system/global install
    Ignore-this: 68a618558b0e59c751ea10b9f7237473
    
    darcs-hash:9c09276590a621b977d6fa1277ae7db4fec80045

--- services/xapian-index/xapian-index.sh
+++ services/xapian-index/xapian-index.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 set -uf -o pipefail
 
-DIR=$MDZ_DIR
-TARGET_DIR=$MDZ_XAPIAN_DIR
-URLPREFIX=$MDZ_XAPIAN_PREFIX
-
 for name in $(ls "$MDZ_DATADIR"); do 
   path="$MDZ_DATADIR/$name"
   a="$path/meta.xml"
-  echo url="$URLPREFIX/$name"
+  echo url="$MDZ_WEBSITE_DIR/$MDZ_WEBSITE_DATA_PREFIX/$name"
   echo name=$name
   xmlstarlet sel -t -m "//species" -o "species= " -v "@sciname" -o "     " -v "@tsn" -o "        " -v "." -n $a | grep -v "^$"
   xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
   echo "sample="`xmlstarlet sel -t -m "//description" -v "." $a`
   echo
-done | scriptindex -v --overwrite $TARGET_DIR $DIR/services/xapian-index/index.def
-
-# rm -rf /var/lib/xapian-omega/data/default                     
-# cp -a $DIR/META/metadb /var/lib/xapian-omega/data/default       
+done | scriptindex -v --overwrite $MDZ_XAPIAN_DIR/$MDZ_XAPIAN_DB $MDZ_DIR/services/xapian-index/index.def
 
 # When using sshfs, the allow_other option must be set, like so:
 # sshfs -o allow_other zeus:/data/genomdata /data/genomdata
