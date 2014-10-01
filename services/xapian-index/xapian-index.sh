0a2ebd1d01481cbcffbd402b464d98a9af8f9074
Ketil Malde <ketil@malde.org>
2014-10-01 12:54:26 +0200

    Warn on missing meta.xml in xapian-index
    Ignore-this: 6dbfbc183740e7ad48fa7f6ab641ccc0
    
    darcs-hash:77b5daa42dd3ab9c4cedc9b9be4df256f1896ef6

--- services/xapian-index/xapian-index.sh
+++ services/xapian-index/xapian-index.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -uf -o pipefail
 
-for name in $(ls "$MDZ_DATADIR"); do 
+. "$MDZ_DIR/functions.sh"
+
+for name in $(ls "$MDZ_DATADIR"); do
+  if [ -f "$MDZ_DATADIR/$name/meta.xml" ]; then
   path="$MDZ_DATADIR/$name"
   a="$path/meta.xml"
   echo url="$MDZ_WEBSITE_DIR/$MDZ_WEBSITE_DATA_PREFIX/$name"
@@ -10,6 +13,9 @@ for name in $(ls "$MDZ_DATADIR"); do
   xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
   echo "sample="`xmlstarlet sel -t -m "//description" -v "." $a`
   echo
+  else
+       warn "$name does not appear to be a valid dataset - skipping"
+  fi
 done | scriptindex -v --overwrite $MDZ_XAPIAN_DIR/$MDZ_XAPIAN_DB $MDZ_DIR/services/xapian-index/index.def
 
 # When using sshfs, the allow_other option must be set, like so:
