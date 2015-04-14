64f8c356273c40f29014b5e45ec30291878012c1
Ketil Malde <ketil@malde.org>
2015-04-14 11:45:05 +0200

    Update xapian-index
    Ignore-this: 540b4a6a24a431ae68eb2e4e21f4169
    
    darcs-hash:f8ed0b182a75d50099bffe897edf0e5cafb63b00

--- services/xapian-index/xapian-index.sh
+++ services/xapian-index/xapian-index.sh
@@ -3,20 +3,15 @@ set -uf -o pipefail
 
 . "$MDZ_DIR/functions.sh"
 
-for name in $(ls "$MDZ_DATADIR"); do
-  if [ -f "$MDZ_DATADIR/$name/meta.xml" ]; then
-  path="$MDZ_DATADIR/$name"
-  a="$path/meta.xml"
+datasets | while read name; do
   echo url="/$MDZ_WEBSITE_DATA_PREFIX/$name"
-  echo name=$name
-  xmlstarlet sel -t -m "//species" -o "species= " -v "@sciname" -o "     " -v "@tsn" -o "        " -v "." -n $a | grep -v "^$"
-  xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n $a  | grep -v "^$"
-  echo "sample="`xmlstarlet sel -t -m "//description" -v "." $a`
+  echo name="$name"
+  a="$(datafile "$name")"
+  xmlstarlet sel -t -m "//species" -o "species= " -v "@sciname" -o "     " -v "@tsn" -o "        " -v "." -n "$a" | grep -v '^$'
+  xmlstarlet sel -t -m "//file" -o "filetype=" -v "@path" -o "   " -v "@mimetype" -n "$a"  | grep -v '^$'
+  echo "sample=$(xmlstarlet sel -t -m "//description" -v "." "$a")"
   echo
-  else
-       warn "$name does not appear to be a valid dataset - skipping"
-  fi
-done | scriptindex -v --overwrite $MDZ_XAPIAN_DIR/$MDZ_XAPIAN_DB $MDZ_DIR/services/xapian-index/index.def
+done | scriptindex -v --overwrite "$MDZ_XAPIAN_DIR/$MDZ_XAPIAN_DB" "$MDZ_DIR/services/xapian-index/index.def"
 
 # When using sshfs, the allow_other option must be set, like so:
 # sshfs -o allow_other zeus:/data/genomdata /data/genomdata
