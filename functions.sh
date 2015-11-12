cd68d3eb5db6c5408a7866bf0ca66446abea22d8
Ketil Malde <ketil@malde.org>
2015-11-12 12:40:55 +0100

    Standardize on three-letter hash prefixes
    Ignore-this: 88d07ab406fd583430c225c39d101454
    
    darcs-hash:bc42a43dcb0a2f7b3c1713b2a311041d5fb980d9

--- functions.sh
+++ functions.sh
@@ -41,7 +41,7 @@ is_dataset(){
 assert_is_dataset(){ is_dataset "$1" || error "'$1' is not a valid dataset."; }
 
 # get path to object
-datafile(){ echo "$MDZ_DATADIR/$1"; }
+datafile(){ echo "$MDZ_DATADIR/${1:0:3}/$1"; }
 
 datasets(){
     find "$MDZ_DATADIR" -type f | while read f; do if is_dataset "$(basename "$f")"; then basename "$f"; fi; done
